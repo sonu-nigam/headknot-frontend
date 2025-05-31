1ed5b4b9e2e5911850027333ee5a323797da0ede
Sonu Nigam <drop@outlook.in>
2025-05-31 15:52:55 +0530

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,5 +3,5 @@ import React from "react";
 type Props = {};
 
 export default function Root({}: Props) {
-    return <div>Root Page</div>;
+    return <div>Root Page {process.env.NEXT_PUBLIC_API_BASE_URL}</div>;
 }
