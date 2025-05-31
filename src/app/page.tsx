b3630000b7d56d0d840347268d80c2945b44556e
Sonu Nigam <drop@outlook.in>
2025-05-31 16:43:20 +0530

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,5 +3,5 @@ import React from "react";
 type Props = {};
 
 export default function Root({}: Props) {
-    return <div>Root Page</div>;
+    return <div>Root Page{process.env.AUTH_SECRET}</div>;
 }
